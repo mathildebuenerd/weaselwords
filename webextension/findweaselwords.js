b93f5e8d3a540f7e732c8e6c7b2d277fbab6f595
mathildebuenerd <mathilde.buenerd@gmail.com>
2019-04-18 21:31:31 +0200

    - translate some comments to english - change from var to let or const - change button visual

--- webextension/findweaselwords.js
+++ webextension/findweaselwords.js
@@ -1,17 +1,17 @@
 
-// Tableau des expressions à trouver
- var numericallyVague = [	"most", "mostly", "of course", "largely", "some", "a lot of", "probably", "countless", "the most common", "or more", "or less", "linked to", "has links to", "scholars", "almost", "certainly", "really", "experts", "many", "often", "it is said", "it is known", "it's said", "it's known", "people say", "it has been", "it's been", "critics", "it stands to reason", "questions have been raised", "experience shows", "may have", "officially", "it turns out", "award-winning", "a recent study", "come to be", "came to be", "up to", "vast majority", "good", "better", "upright", "honorable", "moral", "righteous", "hopefully", "great", "greatest", "grateful", "fantastic", "true", "beautiful", "very", "tough", "highly", "by the way", "bad", "worst", "nasty", "immoral", "evil", "wrong", "corrupt", "wicked", "iniquitous", "unlawful", "naughty", "hateful", "odious", "repugnant", "repellent", "disgusting", "sinful", "harmful", "horrible", "special", "let me tell", "i'll tell you", "believe me", "that's right", "nah", "media", "elite"];
+// Words to find
+let weaselWordList = ["most", "mostly", "of course", "largely", "some", "a lot of", "probably", "countless", "the most common", "or more", "or less", "linked to", "has links to", "scholars", "almost", "certainly", "really", "experts", "many", "often", "it is said", "it is known", "it's said", "it's known", "people say", "it has been", "it's been", "critics", "it stands to reason", "questions have been raised", "experience shows", "may have", "officially", "it turns out", "award-winning", "a recent study", "come to be", "came to be", "up to", "vast majority", "good", "better", "upright", "honorable", "moral", "righteous", "hopefully", "great", "greatest", "grateful", "fantastic", "true", "beautiful", "very", "tough", "highly", "by the way", "bad", "worst", "nasty", "immoral", "evil", "wrong", "corrupt", "wicked", "iniquitous", "unlawful", "naughty", "hateful", "odious", "repugnant", "repellent", "disgusting", "sinful", "harmful", "horrible", "special", "let me tell", "i'll tell you", "believe me", "that's right", "nah", "media", "elite"];
 
- var paragraphe;
- var contentParagraphe;
+let paragraphe;
+let contentParagraphe;
 
 
- var afficheRatio = document.createElement('p');
+let afficheRatio = document.createElement('p');
 
 // statistiques
- var nbWeaselWords = 0;
- var nbLettres = 0;
- var foundWords = [];
+let nbWeaselWords = 0;
+let nbLettres = 0;
+let foundWords = [];
 
 
 createButton();
@@ -19,90 +19,88 @@ createButton();
 
 function createButton() {
 
-	 var myButton = document.createElement('div');
+    // Create the main button and add it to the DOM
+    let myButton = document.createElement('div');
 
-	myButton.setAttribute('id', 'weaselExtensionController');
-	myButton.style.position = "fixed";
-	myButton.style.top = "200px";
-	myButton.style.right = "30px";
-	myButton.style.margin = "0!important";
-	myButton.style.zIndex = "9999999";
-	myButton.style.padding = "10px";
-	myButton.style.backgroundColor = "rgba(255,255,255,0.8)";
-	myButton.style.color = "blue";
-	myButton.style.cursor = "pointer";
-	myButton.style.border = "3px solid blue";
-	myButton.style.fontSize = "15px";
-	myButton.innerHTML = "Find weasel words";
-	myBody = document.getElementsByTagName('div')[0];
-	document.body.insertBefore(myButton,myBody);
-	// alert('fini');
-	myButton.addEventListener('click', setup);	
-	
+    // Add some style 8D
+    Object.assign(myButton.style, {
+        position: "fixed",
+        top: "100px",
+        right: "30px",
+        margin: "0 !important",
+        zIndex: "9999",
+        padding: "15px",
+        backgroundColor: "#30e391",
+        color: "white",
+        cursor: "pointer",
+        boxShadow: "0 3px 5px rgba(0,0,0,0.1)",
+        fontSize: "1.5em",
+    });
+    myButton.setAttribute('id', 'weaselExtensionController');
+
+    myButton.innerHTML = "🙊  Find weasel words";
+    document.body.appendChild(myButton);
+
+    // Add the listener to lauche the analyze when we click the button
+    myButton.addEventListener('click', findWeaselWords);
+
 }
 
-function setup() {
+function findWeaselWords() {
 
-	// Récupère les textes
-	// paragraphe = document.querySelectorAll('p, h1, h2,h3,h4,h5,h6,span,aside, section, div');
-	paragraphe = document.getElementsByTagName('body')[0];
+    // Get all the text of the body
+    // paragraphe = document.querySelectorAll('p, h1, h2,h3,h4,h5,h6,span,aside, section, div');
+    paragraphe = document.getElementsByTagName('body')[0];
 
-	// console.log(paragraphe.innerHTML + "  " + paragraphe.innerHTML.length);
-
-
-		nbLettres+=paragraphe.textContent.length;
+    nbLettres+=paragraphe.textContent.length;
 
 
-	analyse();
-	decouper();
-	statistique();
+    analyze();
+    separateLetters();
+    doStats();
 }
 
 
-function analyse() {
+function analyze() {
+
+    for (const word of weaselWordList) {
+
+        contentParagraphe = paragraphe.innerHTML;
+        console.log("contentParagraphe " + contentParagraphe);
+        const maRegex = new RegExp(word + " ", "gi");
+        let monTableau;
+
+        // Tant que l'on trouve un des mots de la liste dans le paragraphe
+        while ((monTableau = maRegex.exec(contentParagraphe)) !== null) {
+
+            // indexOf doesn't work with regExp so we need to combine substr() and search()
+            const index = contentParagraphe.substr(0).search(maRegex);
+            addBalise(index, word.length, contentParagraphe.length);
+            paragraphe.innerHTML = contentParagraphe;
+
+            let isNew = true;
+
+            for (let i=0; i<foundWords.length; i++) {
+                // si le mot est déjà dans le tableau on incrémente l'occurence
+                if (foundWords[i].word == word) {
+                    foundWords[i].occurence++;
+                    isNew = false;
+                }
+            }
+
+            // si le mot n'a pas été trouvé dans le tableau
+            if (isNew) {
+                foundWords.push(new FoundWord(word));
+            }
+        }
+    }
 
 
-
-	for (var k=0; k<numericallyVague.length;k++) {
-
-			contentParagraphe = paragraphe.innerHTML;
-			console.log("contentParagraphe " + contentParagraphe);
-			var maRegex = new RegExp(numericallyVague[k] + " ", "gi");
-			var monTableau;
-
-			// Tant que l'on trouve un des mots de la liste dans le paragraphe
-			while ((monTableau = maRegex.exec(contentParagraphe)) !== null) {
-
-				// indexOf doesn't work with regExp so we need to combine substr() and search()
-    			var index = contentParagraphe.substr(0).search(maRegex);
-				addBalise(index, numericallyVague[k].length, contentParagraphe.length);
-				paragraphe.innerHTML = contentParagraphe;
-
-				var isNew = true;
-
-				for (var i=0; i<foundWords.length; i++) {
-					// si le mot est déjà dans le tableau on incrémente l'occurence
-					if (foundWords[i].word == numericallyVague[k]) {
-						foundWords[i].occurence++;
-						isNew = false;
-					}
-				}
-
-				// si le mot n'a pas été trouvé dans le tableau
-				if (isNew) {
-					foundWords.push(new FoundWord(numericallyVague[k]));
-				}
-
-			// console.log("numericallyVague[k] " + numericallyVague[k]);
-		}
-	}
-
-
-	// range le tableau en fonction du nombre d'occurence
-	foundWords.sort(function (a, b) {
- 		return b.occurence - a.occurence;
-	});
-	console.log(foundWords);
+    // range le tableau en fonction du nombre d'occurence
+    foundWords.sort(function (a, b) {
+        return b.occurence - a.occurence;
+    });
+    console.log(foundWords);
 }
 
 
@@ -110,14 +108,14 @@ function analyse() {
 
 function addBalise(index, length, textLength) {
 
-			var word = contentParagraphe.slice(index, index + length);
-			var debutTexte = contentParagraphe.slice(0, index);
-			var ouvertureBalise = "<span class='vague'>";
-			var fermetureBalise = "</span>";
-			var suiteTexte = contentParagraphe.slice(index+ length, textLength);
+    const word = contentParagraphe.slice(index, index + length);
+    const debutTexte = contentParagraphe.slice(0, index);
+    const ouvertureBalise = "<span class='vague'>";
+    const fermetureBalise = "</span>";
+    const suiteTexte = contentParagraphe.slice(index+ length, textLength);
 
-			// on assemble le début du texte + la balise + le mot
-			contentParagraphe = debutTexte + ouvertureBalise + word + fermetureBalise + suiteTexte;
+    // on assemble le début du texte + la balise + le mot
+    contentParagraphe = debutTexte + ouvertureBalise + word + fermetureBalise + suiteTexte;
 }
 
 
@@ -126,19 +124,17 @@ function addBalise(index, length, textLength) {
 /****** Animation des lettres ******/
 
 setInterval( function() {
+    const weaselWords = document.querySelectorAll(`.char`);
+    const angleList = ["-30", "-20", "-10", "0", "10", "20", "30"];
 
-		var motsHasardeux = document.getElementsByClassName('char');
-		var listeAngles = ["-30", "-20", "-10", "0", "10", "20", "30"];
-		
-		for(var i=0; i<motsHasardeux.length; i++) {
-			var hasard= Math.ceil(Math.random()*listeAngles.length);
-			
-			motsHasardeux[i].style.display = "inline-block";
-			motsHasardeux[i].style.position = "relative";
-			motsHasardeux[i].style.transition = "2s ease";
-			motsHasardeux[i].style.transform = "rotate(" + listeAngles[hasard-1] + "deg)";
+    for (let i=0; i<weaselWords.length; i++) {
+        const hazard = Math.ceil(Math.random()*angleList.length);
 
-		}
+        weaselWords[i].style.display = "inline-block";
+        weaselWords[i].style.position = "relative";
+        weaselWords[i].style.transition = "2s ease";
+        weaselWords[i].style.transform = "rotate(" + angleList[hazard-1] + "deg)";
+    }
 }, 2000);
 
 
@@ -147,61 +143,61 @@ setInterval( function() {
 
 /******** Découpe les mots ********/
 
-function decouper() {
+function separateLetters() {
 
-	var listeMots = [];
-    array = document.getElementsByClassName('vague');   
+    let listeMots = [];
+    array = document.getElementsByClassName('vague');
 
 // permet d'avoir les mots trouvés mais pas la regex concernée ('most' et 'Most' sont différents);
 // corrigé avec l'utilisation de toUpperCase puis toLowerCase
-for (var k=0; k<array.length; k++) {
-	var mot = array[k].innerHTML.toUpperCase();
-	mot = mot.toLowerCase();
-	listeMots[k] = mot;
-	console.log(listeMots[k]);
-}
-    
+    for (let k=0; k<array.length; k++) {
+        let mot = array[k].innerHTML.toUpperCase();
+        mot = mot.toLowerCase();
+        listeMots[k] = mot;
+        console.log(listeMots[k]);
+    }
 
-    	for (var j=0; j<array.length; j++) {
-			nbWeaselWords += array[j].textContent.length;
-		}
 
-	// console.log("nbWeaselWords " + nbWeaselWords);
+    for (let j=0; j<array.length; j++) {
+        nbWeaselWords += array[j].textContent.length;
+    }
 
-     for (i=0; i<array.length; i++) {
-          expression = array[i].innerHTML; chaine = '';
-          for (var j=0; j<expression.length; j++) {
-               chaine += "<span class='char'>" + expression.substr(j, 1) + "</span>" ; }
-          array[i].innerHTML = chaine; 
-      }
+    // console.log("nbWeaselWords " + nbWeaselWords);
+
+    for (i=0; i<array.length; i++) {
+        expression = array[i].innerHTML; chaine = '';
+        for (let j=0; j<expression.length; j++) {
+            chaine += "<span class='char'>" + expression.substr(j, 1) + "</span>" ; }
+        array[i].innerHTML = chaine;
+    }
 } // decouper
 
 
 
 /******** Donne les statistiques à l'internaute ********/
 
-function statistique() {
-	var initRatio = (nbWeaselWords*100/nbLettres);
-	var ratio = Math.round(initRatio*1000)/1000;
+function doStats() {
+    const initRatio = (nbWeaselWords*100/nbLettres);
+    const ratio = Math.round(initRatio*1000)/1000;
 
-	var myButton = document.getElementById('weaselExtensionController');
+    const myButton = document.querySelector('weaselExtensionController');
 
-	afficheRatio.innerHTML = "Ratio | " + ratio;
-	afficheRatio.innerHTML += "<br/>Most used | " + foundWords[0].word;
-	
-	afficheRatio.style.position = "fixed";
-	afficheRatio.style.right = "30px";
-	afficheRatio.style.zIndex = "9999999";	
-	afficheRatio.style.opacity = "0.9999";
-	afficheRatio.style.top = "300px";
-	afficheRatio.style.color = "red";
-	afficheRatio.style.border = "3px solid red";
-	afficheRatio.style.backgroundColor = "rgba(255,255,255,0.8)";
-	afficheRatio.style.padding = "10px";
-	afficheRatio.style.margin = "0!important";
-	afficheRatio.style.fontSize = "15px";
+    afficheRatio.innerHTML = "Ratio | " + ratio;
+    afficheRatio.innerHTML += "<br/>Most used | " + foundWords[0].word;
 
-	document.body.insertBefore(afficheRatio,myButton);
+    afficheRatio.style.position = "fixed";
+    afficheRatio.style.right = "30px";
+    afficheRatio.style.zIndex = "9999999";
+    afficheRatio.style.opacity = "0.9999";
+    afficheRatio.style.top = "300px";
+    afficheRatio.style.color = "red";
+    afficheRatio.style.border = "3px solid red";
+    afficheRatio.style.backgroundColor = "rgba(255,255,255,0.8)";
+    afficheRatio.style.padding = "10px";
+    afficheRatio.style.margin = "0!important";
+    afficheRatio.style.fontSize = "15px";
+
+    document.body.appendChild(myButton);
 
 
 }
@@ -212,7 +208,7 @@ function statistique() {
 // class FoundWord
 function FoundWord(_word) {
 
-	this.word = _word;
-	this.occurence = 1;
+    this.word = _word;
+    this.occurence = 1;
 
 }
